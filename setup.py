d74391a49cac62c6d20ffa86fa8c88747be10742
michael-m-rosenthal <36268105+michael-m-rosenthal@users.noreply.github.com>
2019-01-07 09:39:26 -0600

    Update setup.py
    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 
 setup (
        name='SIMBA3D',
-       version='1.3.0',
+       version='2.0.0',
        author='Michael M. Rosenthal',
        author_email='Michael.M.Rosenthal@gmail.com',
        url='',
